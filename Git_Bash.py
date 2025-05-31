1e4129359b193acf2bc1c1b6aba68df55d8b7b18
Murilo Rálio <muriloralio8@gmail.com>
2025-05-31 12:11:30 -0300

    Add files via upload
    Código simples, sendo o objetivo, apenas para aprender os comandos do Git Bash.

---
+++ Git_Bash.py
@@ -0,0 +1,8 @@
+#Comandos usados:
+#cd Documents
+#mkdir "Git_Bash"
+#cd Git_Bash
+#touch Git_Bash.py
+#nano Git_Bash.py
+#python Git_Bash.py
+print('Meu primeiro código, usando apenas o Git Bash!')
